565946b7898ab7e35a021710544cf7d84a18503f
Pantelis Zirinis <info@paz-labs.com>
2017-10-23 10:58:31 +0100

    Initial Commit

---
+++ PazMoPubNavigationController/PazMoPubNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  PazMoPubNavigationController.h
+//  PazMoPubNavigationController
+//
+//  Created by Pantelis Zirinis on 23/10/2017.
+//  Copyright © 2017 Pantelis Zirinis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PazMoPubNavigationController.
+FOUNDATION_EXPORT double PazMoPubNavigationControllerVersionNumber;
+
+//! Project version string for PazMoPubNavigationController.
+FOUNDATION_EXPORT const unsigned char PazMoPubNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PazMoPubNavigationController/PublicHeader.h>
+
+
