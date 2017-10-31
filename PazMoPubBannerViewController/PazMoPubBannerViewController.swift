0444c335d2f90aa784c49d8208049bbe031125f7
Pantelis Zirinis <info@paz-labs.com>
2017-10-31 15:42:05 +0000

    Converted to swift. Untested

--- PazMoPubBannerViewController/PazMoPubBannerViewController.swift
+++ PazMoPubBannerViewController/PazMoPubBannerViewController.swift
@@ -11,16 +11,16 @@ import UIKit
 public class PazMoPubBannerViewController: UIViewController {
     
     public enum UpdateNotification {
-        case BannerViewActionWillBegin
-        case BannerViewActionDidFinish
+        case BannerViewActionWillPresent
+        case BannerViewActionDidDismiss
         case BannerViewClassBannerSizeChanged
         case BannerViewClassAdUnitIdChanged
         
         var name: Notification.Name {
             switch self {
-            case .BannerViewActionDidFinish:
+            case .BannerViewActionDidDismiss:
                 return Notification.Name("BannerViewActionDidFinish")
-            case .BannerViewActionWillBegin:
+            case .BannerViewActionWillPresent:
                 return Notification.Name("BannerViewActionWillBegin")
             case .BannerViewClassAdUnitIdChanged:
                 return Notification.Name("BannerViewClassAdUnitIdChanged")
@@ -29,16 +29,36 @@ public class PazMoPubBannerViewController: UIViewController {
             }
         }
     }
-
+    
+    public var contentViewController: UIViewController
+    public lazy var contentView: UIView = {
+        let view = UIView(frame: UIScreen.main.bounds)
+        return view
+    }()
+    private (set) var bannerLoaded: Bool = false
+    private (set) var showing: Bool = false
+    
     // Initialize using a content view controller
     public init(contentViewController: UIViewController, adUnitId: String, active: Bool = true) {
         self.adUnitId = adUnitId
         self.active = active
+        self.contentViewController = contentViewController
+        self.adSize = (UI_USER_INTERFACE_IDIOM() == .pad) ? MOPUB_LEADERBOARD_SIZE : MOPUB_BANNER_SIZE
         super.init(nibName: nil, bundle: nil)
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func loadView() {
+        super.loadView()
+        // Setup containment of the _contentController.
+        self.addChildViewController(self.contentViewController)
+        self.contentView.addSubview(self.contentViewController.view)
+        self.contentViewController.didMove(toParentViewController: self)
+        
+        self.view = self.contentView
     }
     
     // Setting everything in one function
@@ -51,11 +71,19 @@ public class PazMoPubBannerViewController: UIViewController {
     // The MoPub Ad Unit Id to be used to load adverts
     public var adUnitId: String {
         didSet {
+            guard self.adUnitId != oldValue else {
+                return
+            }
             self.removeAdView()
             if self.active {
-                self.adView.loadAd()
+                self.adView?.loadAd()
             }
+            NotificationCenter.default.post(name: UpdateNotification.BannerViewClassAdUnitIdChanged.name, object: self)
         }
+    }
+    
+    public func disableLogging() {
+        MPLogSetLevel(MPLogLevelOff)
     }
     
     // Activates/Deactivate the presentation of adverts
@@ -65,21 +93,25 @@ public class PazMoPubBannerViewController: UIViewController {
                 return
             }
             if self.active {
-                self.adView.loadAd()
+                self.adView?.loadAd()
             } else {
                 self.removeAdView()
             }
         }
     }
     
-    // Whether an advert is currently visible
-    var showing: Bool {
-        return !self.isToolbarHidden
-    }
-    
     // Ad Size for current device
     public var adSize: CGSize {
-        return (UI_USER_INTERFACE_IDIOM() == .pad) ? MOPUB_LEADERBOARD_SIZE : MOPUB_BANNER_SIZE
+        didSet {
+            guard self.adSize != oldValue else {
+                return
+            }
+            if self.adSize == CGSize.zero {
+                self.removeAdView()
+                let _ = self.adView
+            }
+            NotificationCenter.default.post(name: UpdateNotification.BannerViewClassBannerSizeChanged.name, object: self)
+        }
     }
     
     // MARK: - View Updates
@@ -95,25 +127,29 @@ public class PazMoPubBannerViewController: UIViewController {
     }
     
     public func refreshLayout() {
-        guard let adView = self._adView else {
+        var contentFrame = self.view.bounds
+        guard var bannerFrame = self.adView?.frame else {
             return
         }
         
-        var toolbarFrame = self.toolbar.frame
-        
-        let size = adView.adContentViewSize()
-        var newFrame = self.adView.frame
-        newFrame.size = size;
-        newFrame.origin.x = (toolbarFrame.size.width - size.width) / 2
-        adView.frame = newFrame;
-        
-        let dHeight = toolbarFrame.size.height - self.adSize.height
-        guard dHeight != 0.0 else {
-            return
+        // Check if the banner has an ad loaded and ready for display.  Move the banner off
+        // screen if it does not have an ad.
+        if self.bannerLoaded {
+            contentFrame.size.height -= bannerFrame.size.height
+            bannerFrame.origin.y = contentFrame.size.height
+            self.showing = true
+        } else {
+            bannerFrame.origin.y = contentFrame.size.height
+            self.showing = false
         }
-        toolbarFrame.size.height -= dHeight;
-        toolbarFrame.origin.y += dHeight;
-        self.toolbar.frame = toolbarFrame;
+        self.contentViewController.view.frame = contentFrame
+        if let nv = self.contentViewController as? UINavigationController {
+            nv.visibleViewController?.view.setNeedsLayout()
+        }
+        // Center banner frame
+        // Repositioning example: keep the ad view centered horizontally.
+        bannerFrame.origin.x = (self.view.bounds.size.width - bannerFrame.size.width) / 2;
+        self.adView?.frame = bannerFrame;
     }
     
     // MARK: - AdView
@@ -123,35 +159,89 @@ public class PazMoPubBannerViewController: UIViewController {
     }
     
     private var _adView: MPAdView?
-    public var adView: MPAdView {
+    public var adView: MPAdView? {
+        guard self.active else {
+            return nil
+        }
         if let adView = self._adView {
             return adView
         }
         let adView = self.newAdView()
         adView.delegate = self
-        self.view.needsUpdateConstraints()
-        self.toolbar.addSubview(adView)
+        self.contentView.addSubview(adView)
+        self._adView = adView
         return adView
     }
     
     func removeAdView() {
+        guard self._adView != nil else {
+            self.bannerLoaded = false
+            return
+        }
+
         self._adView?.removeFromSuperview()
         self._adView = nil
-        self.isToolbarHidden = true
+        self.bannerLoaded = false
+        UIView.animate(withDuration: 0.25) {
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
 extension PazMoPubBannerViewController: MPAdViewDelegate {
     public func viewControllerForPresentingModalView() -> UIViewController! {
-        return self.visibleViewController
+        return self.contentViewController
     }
     
     public func adViewDidLoadAd(_ view: MPAdView!) {
-        self.isToolbarHidden = false
-        self.view.setNeedsLayout()
+        self.showing = true
+        UIView.animate(withDuration: 0.25) {
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+        }
     }
     
     public func adViewDidFail(toLoadAd view: MPAdView!) {
-        self.isToolbarHidden = true
+        guard let _ = self._adView else {
+            self.bannerLoaded = false
+            return
+        }
+        self.bannerLoaded = false
+        UIView.animate(withDuration: 0.25) {
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+        }
     }
+    
+    public func willPresentModalView(forAd view: MPAdView!) {
+        NotificationCenter.default.post(name: UpdateNotification.BannerViewActionWillPresent.name, object: self)
+    }
+    
+    public func didDismissModalView(forAd view: MPAdView!) {
+        NotificationCenter.default.post(name: UpdateNotification.BannerViewActionDidDismiss.name, object: self)
+    }
+
 }
+
+// MARK: - Interface Orientation and Rotation
+public extension PazMoPubBannerViewController {
+    public override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        self.adView?.rotate(to: toInterfaceOrientation)
+    }
+    
+    public override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
+    }
+    
+    public override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return self.contentViewController.preferredInterfaceOrientationForPresentation
+    }
+    
+    public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return self.contentViewController.supportedInterfaceOrientations
+    }
+    
+}
+
